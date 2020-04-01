02d0a2a4d74121676bddc80bdbb6af8f07dde0f3
Thiago Nonato Antunes <thiagotna@hotmail.com>
2020-04-01 13:41:45 -0300

    Update module_datetime.py

--- aulas/module_datetime.py
+++ aulas/module_datetime.py
@@ -1,15 +1,34 @@
 # https://docs.python.org/3/library/datetime.html?highlight=strftime#datetime.datetime.strftime
-import os
 import datetime
+import pytz
 
 my_date = datetime.datetime.today()
-print(my_date)
-
 sentence = f'{my_date:%d %B, %Y }'
-print(sentence)
-
 formated_date = f'{my_date:%B %d, %Y fell on %A and was the %j day of the year}'
-print(formated_date)
 
-mod_time = os.stat('abc.txt').st_mtime  #-> 1585084202.5290809
-print(datetime.datetime.fromtimestamp(mod_time))  # modify timestamp
+# mod_time = os.stat('abc.txt').st_mtime  #-> 1585084202.5290809
+# print(datetime.datetime.fromtimestamp(mod_time))  # modify timestamp
+
+dt = datetime.datetime(2020, 4, 1, 12, 14, 45, tzinfo=pytz.UTC)
+# print(dt)
+
+dt_today = datetime.datetime.today() 
+dt_utcnow = datetime.datetime.now(tz=pytz.UTC)
+dt_sp = dt_utcnow.astimezone(pytz.timezone('America/Sao_Paulo'))
+dt_now = datetime.datetime.now(tz=pytz.timezone('America/Sao_Paulo'))
+
+print(dt_now.strftime('%B %d, %Y'))
+dt_str = 'April 01, 2020'
+dt = datetime.datetime.strptime(dt_str, '%B %d, %Y')
+print(dt)
+
+#  strftime - Datetime to String
+#  strptime - String to Datetime
+
+# for tz in pytz.all_timezones:
+#     print(tz)
+
+# print(dt_sp)
+# print(dt_today)
+# print(dt_now)
+# print(dt_utcnow)
