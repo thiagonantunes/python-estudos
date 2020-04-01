846f11d7f4f92e0061823c78ce17b8fba3bcbe25
Thiago Nonato Antunes <thiagotna@hotmail.com>
2020-04-01 07:17:14 -0300

    Upgrade

--- aulas/classes/property_setter.py
+++ aulas/classes/property_setter.py
@@ -1,29 +1,29 @@
 class Employee:
 
-  def __init__ (self, first, last):
-    self.first = first
-    self.last = last
-    # self.email = f'{first}.{last}@company.com'  #first + '.' + last + '@email.com'
+    def __init__ (self, first, last):
+      self.first = first
+      self.last = last
+      # self.email = f'{first}.{last}@company.com'  #first + '.' + last + '@email.com'
 
   @property
   def email(self):
-    return f'{self.first}.{self.last}@company.com'
+      return f'{self.first}.{self.last}@company.com'
 
   @property
   def fullname(self):
-    return f'{self.first} {self.last}'
+      return f'{self.first} {self.last}'
 
   @fullname.setter
   def fullname(self, name):
-    first, last = name.split(' ')
-    self.first = first
-    self.last = last
+      first, last = name.split(' ')
+      self.first = first
+      self.last = last
 
   @fullname.deleter
   def fullname(self):
-    print('Delete name!')
-    self.first = None
-    self.last = None
+      print('Delete name!')
+      self.first = None
+      self.last = None
 
 emp_1 = Employee('John', 'Smith')
 emp_1.fullname = 'Corey Schafer'
