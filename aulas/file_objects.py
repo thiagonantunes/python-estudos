ed63da80a3a4440592d24e26bf3865396cbb5f4a
Thiago Nonato Antunes <thiagotna@hotmail.com>
2020-04-01 16:10:14 -0300

    file objects

---
+++ aulas/file_objects.py
@@ -0,0 +1,20 @@
+# File objects
+
+with open('abc.txt') as f:
+    f_contents = f.read()
+    print(f_contents)
+
+
+
+
+
+# with open('abc.txt', 'w+') as file:
+#     file.write('Linha1\n')
+#     file.write('Linha2\n')
+#     file.write('Linha4\n')
+
+#     file.seek(0, 0)  # começa ler da primeira linha, primeiro caractere
+#     print('Lendo linhas:')
+    
+#     file.close()
+
